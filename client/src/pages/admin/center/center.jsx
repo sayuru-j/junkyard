71301d1b9e89a99c3f212415d6392d9c5118f30b
Sayuru <zayuru.ai@gmail.com>
2023-05-18 21:44:16 +0530

    Update center.jsx

--- client/src/pages/admin/center/center.jsx
+++ client/src/pages/admin/center/center.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import CenterList from "../../../components/center/CenterList";
+import { ViewGridAddIcon } from "@heroicons/react/outline";
 
 function Center() {
   return (
     <div className="mt-5 min-h-screen">
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center">
-          <h1 className="text-lg font-medium text-primary">Center Dashboard</h1>
+          <h1 className="text-lg font-bold text-primary">Center Dashboard</h1>
         </div>
 
         <div>
-          <a className="bg-primary px-4 py-1 text-white text-sm font-medium rounded-lg shadow-sm">
-            CREATE NEW
+          <a
+            href="/AddCenter"
+            className="bg-primary/70 hover:bg-primary cursor-pointer px-4 py-1 text-white text-sm font-medium rounded-lg shadow-sm inline-flex gap-2 items-center transition-all duration-200 ease-out"
+          >
+            Add Centers
+            <span>
+              <ViewGridAddIcon className="w-5" />
+            </span>
           </a>
         </div>
       </div>

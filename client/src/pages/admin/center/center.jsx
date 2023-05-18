9ac1d865672bc1784136db7f0acf1c59fa925d96
Sayuru <zayuru.ai@gmail.com>
2023-05-18 21:00:19 +0530

    Update center.jsx

--- client/src/pages/admin/center/center.jsx
+++ client/src/pages/admin/center/center.jsx
@@ -3,24 +3,17 @@ import CenterList from "../../../components/center/CenterList";
 
 function Center() {
   return (
-    <div className="mt-5">
+    <div className="mt-5 min-h-screen">
       <div className="flex w-full items-center justify-between">
-        <div className="flex gap-1 items-center">
+        <div className="flex items-center">
           <h1 className="text-lg font-medium text-primary">Center Dashboard</h1>
-          <p className="text-xs font-thin text-primary/30">
-            Here you can add or remove existing centers.
-          </p>
         </div>
 
         <div>
-          <a className="bg-primary px-4 py-1 text-white text-sm font-medium rounded-lg">
+          <a className="bg-primary px-4 py-1 text-white text-sm font-medium rounded-lg shadow-sm">
             CREATE NEW
           </a>
         </div>
-      </div>
-
-      <div>
-        <h1 className="text-sm">Total: 14 Centers</h1>
       </div>
 
       <div>

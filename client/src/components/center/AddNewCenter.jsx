d8a7c7d1c5aec0b7db68cd1d7e9afe62485e6a20
Sayuru <zayuru.ai@gmail.com>
2023-05-18 21:44:11 +0530

    Update AddNewCenter.jsx

--- client/src/components/center/AddNewCenter.jsx
+++ client/src/components/center/AddNewCenter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function AddNewCenter() {
-  return <div>AddNewCenter</div>;
+  return <div className="min-h-screen pt-10">AddNewCenter</div>;
 }
 
 export default AddNewCenter;

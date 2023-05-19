1cb6b6bc37bb04ac345849c589d2c0efb303fd73
Sayuru <zayuru.ai@gmail.com>
2023-05-19 22:35:58 +0530

    Update fleet.jsx

--- client/src/pages/admin/fleet/fleet.jsx
+++ client/src/pages/admin/fleet/fleet.jsx
@@ -1,26 +1,19 @@
-import Header from "../../../components/Header"
-import Footer from "../../../components/Footer"
-import VehicleAdd from "../../../components/fleet/VehicleAdd"
-import Layout from "../../../components/Layout"
-import VehicleList from "../../../components/fleet/VehicleList"
-import SheduleList from "../../../components/fleet/SheduleList"
-import SheduleAdd from "../../../components/fleet/SheduleAdd"
-import UpdateVehicle from "../../../components/fleet/UpdateVehicle"
+import VehicleAdd from "../../../components/fleet/VehicleAdd";
+import VehicleList from "../../../components/fleet/VehicleList";
+import SheduleList from "../../../components/fleet/SheduleList";
+import SheduleAdd from "../../../components/fleet/SheduleAdd";
+import UpdateVehicle from "../../../components/fleet/UpdateVehicle";
 
 function Fleet() {
   return (
     <>
-    <Layout>
-    <Header/>
-    <VehicleAdd/>
-    <SheduleAdd/>
-    <UpdateVehicle/>
-    <SheduleList/>
-    <VehicleList/>
-    </Layout>
-    <Footer/>
+      <VehicleAdd />
+      <SheduleAdd />
+      <UpdateVehicle />
+      <SheduleList />
+      <VehicleList />
     </>
-  )
+  );
 }
 
-export default Fleet
+export default Fleet;

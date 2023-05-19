53fe44ce226f7c7246938aab939e8ed7210bb43a
Sayuru <zayuru.ai@gmail.com>
2023-05-19 17:30:29 +0530

    Create register.jsx

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,158 @@
+import { useEffect, useState } from "react";
+import logo from "../assets/logo.png";
+import axios from "axios";
+import Success from "../utils/Success";
+import { useNavigate } from "react-router-dom";
+import Error from "../utils/Error";
+import useTimeout from "../utils/CustomHooks/useTimeout";
+
+function Register() {
+  const navigate = useNavigate();
+  const [state, setState] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleChange = (item) => (e) => {
+    setState({
+      ...state,
+      [item]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API}/register`,
+        {
+          username: state.username,
+          name: state.name,
+          email: state.email,
+          password: state.password,
+        }
+      );
+
+      setSuccess(response.data.success);
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
+
+  return (
+    <section className="">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="/"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+        >
+          <img className="w-14 mr-2 object-cover" src={logo} alt="logo" />
+          Junkyard
+        </a>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="John Doe"
+                  required={true}
+                  onChange={handleChange("name")}
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="johndoe"
+                  required={true}
+                  onChange={handleChange("username")}
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@email.com"
+                  required={true}
+                  onChange={handleChange("email")}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                  onChange={handleChange("password")}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Sign in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  href="/login"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign in
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+      {success && <Success message={success} />}
+      {error && <Error message={error} />}
+    </section>
+  );
+}
+
+export default Register;

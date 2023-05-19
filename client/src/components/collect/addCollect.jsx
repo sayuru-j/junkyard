f3ac43a42c18fef5be8090b081b6ba26ce23f767
raninduSachintha <it20611774@my.sliit.lk>
2023-05-20 05:15:40 +0530

    Update addCollect.jsx

--- client/src/components/collect/addCollect.jsx
+++ client/src/components/collect/addCollect.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import {Paper, Button} from "@mui/material"
 import React, { useEffect, useState } from 'react'
 import {
   TrashIcon,
@@ -48,83 +47,41 @@ function AddCollection() {
   }
 
   return (
-    <div className="vh"> 
-            {JSON.stringify(state)}
-        <div className="container ">
-        <h5 className="text-os text-center">Add New Product</h5>
-            <div className="card-pad">
-            <Paper elevation={1}>
-            <div className="row justify-content-center card-pad">
-            <div className="col" 
-                style={{backgroundImage: 'url(https://source.unsplash.com/random)',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        maxWidth: '450px',
-                        marginLeft: '10px'
-                
-                }}>
-                    
-            </div>
-            <div className="col-sm d-flex justify-content-center">
+    <div className='min-h-screen grid grid-cols-4 pt-20'>
+    <div>
 
-            <form className= "col-md-8 mt-4 mx-auto" onSubmit={handleSubmit}> 
-                <div className="form-cus">
-
-                <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Customer name</label>               
-                <input  
-                onChange={handleChange("name")}
-                type="text" className="form-control font-size-sm" id="exampleFormControlInput1" placeholder="ex:  John sins"
-                     
-                />          
-                </div>
-
-                <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Address</label>
-                <input  type="text" className="form-control font-size-sm" id="exampleFormControlInput1" placeholder="ex:  No:17/3, sk Road, colombo"
-                      onChange={handleChange("address")}
-                />
-                </div>
-
-                <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Collection Date</label>
-                <input 
-                onChange={handleChange("collectionDate")}
-                type="datetime-local" className="form-control font-size-sm" id="exampleFormControlInput1" placeholder="ex:  2023/02/10"
-                        
-                />
-                </div>
-
-                
-
-                <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Waste Type</label>
-                <input  
-                onChange={handleChange("wasteType")}
-                type="text" className="form-control font-size-sm" id="exampleFormControlInput1" placeholder="ex:   plastic"
-                        
-                />
-                </div>
-
-                <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Quantity</label>
-                <input type="text" className="form-control font-size-sm" id="exampleFormControlInput1" placeholder="Product Details"
-                        onChange={handleChange("quantity")}
-                />
-                </div>
-
-                      
-                           <Button  type="submit" variant="outlined" color="success" >Submit</Button>
-                      
-                </div>
-                </form>
-
-                </div>
-                </div>
-            </Paper>
-            </div>
-        
+    </div>
+    <div className='col-span-3'>
+    <form class="w-full max-w-lg">
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        Name
+      </label>
+      <input 
+      onChange={handleChange("name")}
+      class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
+      <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+        Address
+      </label>
+      <input
+      onChange={handleChange("address")} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
+    </div>
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+        Collection Date
+      </label>
+      <input onChange={handleChange("collectionDate")} class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"/>
+      <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+    </div>
+  </div>
+  
+</form>
     </div>
     </div>
   )

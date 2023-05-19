01cfdec2635b5c6dec8f91327207b8ca11c37cc0
Sayuru <zayuru.ai@gmail.com>
2023-05-19 17:30:16 +0530

    Create AccessDenied.jsx

---
+++ client/src/components/AccessDenied.jsx
@@ -0,0 +1,9 @@
+function AccessDenied() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-sm">Access Denied</h1>
+    </div>
+  );
+}
+
+export default AccessDenied;

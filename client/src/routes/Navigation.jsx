47c43a0c2f4db708407b9d5eec616e5fe304ba26
Sayuru <zayuru.ai@gmail.com>
2023-05-19 17:30:30 +0530

    Update Navigation.jsx

--- client/src/routes/Navigation.jsx
+++ client/src/routes/Navigation.jsx
@@ -1,19 +1,45 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
+import Register from "../pages/register";
+import Login from "../pages/login";
 import Center from "../pages/admin/center/center";
 import AddNewCenter from "../components/center/AddNewCenter";
 import UpdateCenter from "../components/center/UpdateCenter";
+import AccessDenied from "../components/AccessDenied";
+import { useEffect, useState } from "react";
 
 function Navigation() {
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    setUserData(JSON.parse(localStorage.getItem("userData")));
+  }, []);
+
   return (
     <>
       <Routes>
         <Route path="/" Component={Home} />
 
+        {/* Login Routes */}
+        <Route path="/register" Component={Register} />
+        <Route path="/login" Component={Login} />
+
         {/* Center Routes */}
-        <Route path="/ManageCenters" Component={Center} />
-        <Route path="/AddCenter" Component={AddNewCenter} />
-        <Route path="/UpdateCenter/:id" Component={UpdateCenter} />
+        <Route
+          path="/ManageCenters"
+          Component={userData?.user?.role === "admin" ? Center : AccessDenied}
+        />
+        <Route
+          path="/AddCenter"
+          Component={
+            userData?.user?.role === "admin" ? AddNewCenter : AccessDenied
+          }
+        />
+        <Route
+          path="/UpdateCenter/:id"
+          Component={
+            userData?.user?.role === "admin" ? UpdateCenter : AccessDenied
+          }
+        />
       </Routes>
     </>
   );

44a4aced891b396c5a0e5ced066f8186fb7be29e
Sayuru <zayuru.ai@gmail.com>
2023-05-19 21:18:51 +0530

    Update centerValidator.js

--- server/validators/centerValidator.js
+++ server/validators/centerValidator.js
@@ -52,3 +52,56 @@ exports.addNewCenterValidator = [
         .isLength({ max: 100})
         .withMessage('Address cannot be exceed 100 characters')
 ]
+
+exports.updateCenterValidator = [
+    check('center_Id')
+        .not()
+        .isEmpty()
+        .isLength({ min: 6, max: 10})
+        .withMessage('Center Id must be between 6 and 10 characters'),
+
+    check('center_Name')
+        .not()
+        .isEmpty()
+        .withMessage('Center Name is required'),
+
+    check('address')
+        .not()
+        .isEmpty()
+        .withMessage('Address is required'),
+
+    check('supervisor')
+        .not()
+        .isEmpty()
+        .withMessage('Supervisor Name is required'),
+
+    check('phone_No')
+        .isNumeric()
+        .withMessage('Phone Number is required'),
+
+    check('email')
+        .isEmail()
+        .withMessage('Email is required'),
+
+    check('operating_Hours')
+        .not()
+        .isEmpty()
+        .isLength({ min: 3 })
+        .withMessage('Operating Hours is required'),
+
+    check('accepted_Materials')
+        .not()
+        .isEmpty()
+        .withMessage('Select at least one accepted materials'),
+
+    check('photo_Url')
+        .not()
+        .isEmpty()
+        .withMessage('Photo is required'),
+
+    check('services_Offered')
+        .not()
+        .isEmpty()
+        .isLength({ max: 100})
+        .withMessage('Address cannot be exceed 100 characters')
+]

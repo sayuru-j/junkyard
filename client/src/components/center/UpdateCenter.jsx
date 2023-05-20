c87c9268ab2ad3fded9dd8bccf933b81d1a2ced0
Sayuru <zayuru.ai@gmail.com>
2023-05-20 12:42:25 +0530

    Update UpdateCenter.jsx

--- client/src/components/center/UpdateCenter.jsx
+++ client/src/components/center/UpdateCenter.jsx
@@ -120,7 +120,7 @@ function UpdateCenter() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col mt-10 gap-5">
+    <div className="min-h-screen flex flex-col mt-10 gap-5 mb-20">
       <div className="flex items-center text-primary gap-1">
         <h1 className="pl-1 font-medium text-lg">Adding A New Center</h1>
         <PlusCircleIcon className="w-5 animate-spin" />
